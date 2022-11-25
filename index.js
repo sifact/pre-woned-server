48296c0855492a7d1894d1d10a2de0098abb8a58
sifat <Sifatami596@gmail.com>
2022-11-25 19:29:57 +0600

    category api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const cors = require("cors");
 // const jwt = require("jsonwebtoken");
@@ -11,13 +12,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// async function run() {
-//     try {
-//     } finally {
-//     }
-// }
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xloj4nu.mongodb.net/?retryWrites=true&w=majority`;
 
-// run().catch((e) => console.log(e));
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const categoriesCollection = client
+            .db("reseller")
+            .collection("categories");
+
+        const productsCollection = client.db("reseller").collection("products");
+
+        app.get("/categories", async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        });
+
+        // get specific category products
+
+        app.get("category/:id", async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const category = await productsCollection.findOne(query);
+            res.send(category);
+        });
+    } finally {
+    }
+}
+
+run().catch((e) => console.log(e));
 app.get("/", async (req, res) => {
     res.send("reseller server is running");
 });
